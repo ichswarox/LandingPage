06e9914ca92c0c4271130c64b09dc0ce92c5b90c
ich <xhyxx25@gmail.com>
2025-09-02 00:20:54 +0800

    google ads

--- LandingPageHoudini/server.js
+++ LandingPageHoudini/server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// 设置Content Security Policy头
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://client.crisp.chat https://sdk.51.la; object-src 'none'");
+  next();
+});
+
 // 静态文件服务
 app.use(express.static(path.join(__dirname, 'public')));
 
